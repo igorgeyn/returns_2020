fab2d0b66d27183fdc84901b99c37fbd035961f5
Igor Geyn <igorgeyn@gmail.com>
2020-11-04 22:13:14 -0800

    made it work in one script

--- nyt_county_results_master_df.py
+++
@@ -1,7 +0,0 @@
-df_all_states_final = pd.DataFrame()
-
-for index, c in enumerate(csvs_list):
-    df = pd.read_csv(csvs_list[index])
-    df_all_states_final = df_all_states_final.append(df)
-
-df_all_states_final.to_csv('df_all_states_final.csv')
